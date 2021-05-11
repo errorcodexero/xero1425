48e368812f334f77c2311faecb213959a2396990
Jack (Butch) Griffin <butchg@comcast.net>
2021-05-11 10:17:20 -0700

    added the ability for the OISubsystem base class to add the gamepad automatically

--- base/oi/OISubsystem.java
+++ base/oi/OISubsystem.java
@@ -7,12 +7,53 @@ import org.xero1425.base.LoopType;
 import org.xero1425.base.Subsystem;
 import org.xero1425.base.actions.InvalidActionRequest;
 import org.xero1425.base.actions.SequenceAction;
+import org.xero1425.base.tankdrive.TankDriveSubsystem;
+import org.xero1425.misc.BadParameterTypeException;
+import org.xero1425.misc.MessageLogger;
+import org.xero1425.misc.MessageType;
+import org.xero1425.misc.MissingParameterException;
 
 public class OISubsystem extends Subsystem {
-    public OISubsystem(Subsystem parent, String name) {
+    
+    private List<HIDDevice> devices_ ;
+    private TankDriveGamepad gp_ ;
+    
+    private final static String DriverGamepadHIDIndexName = "hw:driverstation:hid:driver";
+
+    public OISubsystem(Subsystem parent, String name, TankDriveSubsystem db) {
         super(parent, name);
 
         devices_ = new ArrayList<HIDDevice>();
+
+        if (db != null) {           
+            MessageLogger logger = getRobot().getMessageLogger() ;
+            int index;
+
+            try {
+                index = getRobot().getSettingsParser().get(DriverGamepadHIDIndexName).getInteger();
+            } catch (BadParameterTypeException e) {
+                logger.startMessage(MessageType.Error) ;
+                logger.add("parameter ").addQuoted(DriverGamepadHIDIndexName) ;
+                logger.add(" exists, but is not an integer").endMessage();
+                index = -1 ;
+            } catch (MissingParameterException e) {
+                logger.startMessage(MessageType.Error) ;
+                logger.add("parameter ").addQuoted(DriverGamepadHIDIndexName) ;
+                logger.add(" does not exist").endMessage();
+                index = -1 ;            
+            }
+            
+            if (index != -1) {
+                try {
+                    gp_ = new TankDriveGamepad(this, index, db) ;
+                    addHIDDevice(gp_);
+                }
+                catch(Exception ex) {
+                    logger.startMessage(MessageType.Error) ;
+                    logger.add("driver gamepad HID device was not created ").endMessage();
+                }
+            }
+        }
     }
 
     /// \brief returns true if this is the OI subsystem
@@ -69,6 +110,4 @@ public class OISubsystem extends Subsystem {
     protected void addHIDDevice(HIDDevice dev) {
         devices_.add(dev) ;
     }
-
-    private List<HIDDevice> devices_ ;
 } ;
