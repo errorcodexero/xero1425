6f4afd39134d03b59fd9e7159463cb9ce4abd3b2
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-01-25 20:32:34 -0800

    added turret offset

--- base/utils/TargetTracker.java
+++ base/utils/TargetTracker.java
@@ -9,7 +9,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 public class TargetTracker {
     
     Translation2d target_ = new Translation2d() ;
-    // TODO: support turret offset in further math calculations.
     Translation2d turret_offset_ = new Translation2d() ;
 
     // \param target the position of the goal/target on the field
@@ -32,9 +31,16 @@ public class TargetTracker {
         //  Gtheta = arctan((Gy - Ty)/(Gx - Tx))
         //  Ttheta = Gtheta - Rtheta
 
-        Translation2d rel_target_ = target_.minus(robot.getTranslation()) ;
+        // to account for turret offset
+        Translation2d rotate_turret_ = turret_offset_.rotateBy(robot.getRotation()) ;
+        Translation2d start_ = rotate_turret_.plus(robot.getTranslation()) ;
+        Pose2d turret_center_ = new Pose2d(start_, robot.getRotation()) ;
+
+        // turret center should = robot center in the case that turret offset == 0
+        Translation2d rel_target_ = target_.minus(turret_center_.getTranslation()) ;
         Rotation2d rel_target_angle_ = new Rotation2d(rel_target_.getX(), rel_target_.getY()) ;
 
+        // making it degrees
         double rel_target_angle_degrees_ = rel_target_angle_.getDegrees() - robot.getRotation().getDegrees() ;
             
         return XeroMath.normalizeAngleDegrees(rel_target_angle_degrees_) ;
@@ -44,6 +50,11 @@ public class TargetTracker {
     public double getRelativeTargetDistance(Pose2d robot) {
 
         return robot.getTranslation().getDistance(target_);
+    }
+
+    // \returns turret offset
+    private Translation2d getTurretOffset() {
+        return turret_offset_ ;
     }
 
 }
